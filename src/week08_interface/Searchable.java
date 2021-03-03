710f4f126a4432738b56f85008bce191ca9e3566
JUHONG PARK <corallines@naver.com>
2021-03-03 11:51:56 +0900

    setMute 함수 추가 - 다중 implements

--- src/week08_interface/Searchable.java
+++ src/week08_interface/Searchable.java
@@ -2,4 +2,13 @@ package week08_interface;
 
 public interface Searchable {
     void search(String url);
+    
+    default void setMute(boolean mute) { 
+        if (mute) {
+            System.out.println("Searchable - 무음 처리");
+        }
+        else {
+            System.out.println("Searchable - 무음 해제");
+        }
+    }
 }
